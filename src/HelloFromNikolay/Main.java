6a5470530da8cf8d8761d2d9e9f3ea3869cad405
Kimberlyka24 <kimberlyka24@gmail.com>
2021-01-30 19:19:26 +0800

    Hello From Kim

--- src/HelloFromNikolay/Main.java
+++ src/HelloFromNikolay/Main.java
@@ -3,5 +3,6 @@ package HelloFromNikolay;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Nikolay");
+	System.out.println("Hello Nikolay, my name is Kim");
     }
 }
