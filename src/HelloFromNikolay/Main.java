cb86c0691b459fe2bdca404af82f39733f2f7bb8
slacama <sariahacama@gmail.com>
2021-02-01 15:35:24 +0800

    update from Kate

--- src/HelloFromNikolay/Main.java
+++ src/HelloFromNikolay/Main.java
@@ -3,6 +3,7 @@ package HelloFromNikolay;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Nikolay");
-	System.out.println("Hello Nikolay, my name is Kim");
+	    System.out.println("Hello Nikolay, my name is Kim");
+	    System.out.println("Hello Nikolay, my name is Kate.");
     }
 }
